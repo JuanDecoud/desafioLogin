06fa5f014f22d10c6a7d0016082db3099754fb0a
JuanDecoud <juanjo.decoud@gmail.com>
2023-09-05 09:50:48 -0300

    PERMISOS

--- src/routers/carts.router.js
+++ src/routers/carts.router.js
@@ -1,16 +1,17 @@
 import { Router } from 'express'
 import CartController from '../controllers/cart.controller.js'
+import { comprobateAdmin, comprobateLoggueUser, comprobateUser } from '../middlewares/user.middleware.js'
 
 const cartsRouter = Router ()
 
 const cartController = new CartController ()
 
 
-cartsRouter.post ('/:cid/products/:pid' ,cartController.updateCart )
-cartsRouter.post ('/',cartController.createCart)
-cartsRouter.post('/:cid/purchase' ,cartController.finishPurchase )
-cartsRouter.get('/:cid',cartController.findCart)
-cartsRouter.delete('/:cid/product/:pid', cartController.deleteProduct)
+cartsRouter.post ('/:cid/products/:pid' ,comprobateLoggueUser,comprobateUser ,cartController.updateCart )
+cartsRouter.post ('/',comprobateLoggueUser,comprobateUser,cartController.createCart)
+cartsRouter.post('/:cid/purchase' ,comprobateLoggueUser,comprobateUser ,cartController.finishPurchase )
+cartsRouter.get('/:cid',comprobateLoggueUser,comprobateAdmin,cartController.findCart)
+cartsRouter.delete('/:cid/product/:pid',comprobateLoggueUser,comprobateUser ,cartController.deleteProduct)
 cartsRouter.put('/:cid' , cartController.putProductArray)
 
 
