a05904487f238c7b0a3529d28b357a46f89c0c54
JuanDecoud <juanjo.decoud@gmail.com>
2023-09-07 13:06:58 -0300

    aqsd

--- src/middlewares/errors.middleware.js
+++ src/middlewares/errors.middleware.js
@@ -1,10 +1,9 @@
 import EErros from "../services/errors/enums.js";
 
 export default (error, req, res, next) => {
-    
     switch (error.code) {
         case EErros.NO_STOCK:
-            res.status(400).send({ status: 'error', error: error.name})
+            res.status(400).json({ status: 'error', error: error.name})
             break;
         default:
             res.send({ status: 'error', error: 'Unhandled error'})
